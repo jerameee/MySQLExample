436926eff8c498d62801c6c51db369a504b4ac2b
Jeremy <jeremyfortenberry@Jeremys-MacBook-Pro.local>
2019-07-02 11:27:42 -0700

    Initial commit

---
+++ app/src/main/java/com/example/mysqlexample/DBStrings.java
@@ -0,0 +1,9 @@
+package com.example.mysqlexample;
+
+public class DBStrings {
+
+    static final String DATABASE_URL = "127.0.0.1:3306";
+    static final String DATABASE_NAME = "MySQL_Example_(Android)";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "mysql480";
+}
